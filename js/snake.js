aaf0a2404cec49df0f53e5fe91cd74bd4527fc28
Ashley McDonald <ashley.mcdonald@frhsd.com>
2023-02-14 10:56:21 -0500

    21:23 in video

--- js/snake.js
+++ js/snake.js
@@ -1,10 +1,17 @@
 export const SNAKE_SPEED = 1;
-const snakeBody = [{ x : 11, y : 11 }];
+const snakeBody = [
+    { x : 10, y : 11 },
+    { x : 11, y : 11 },
+    { x : 12, y : 11 }
+];
 
 // Update the snake
 export function update()
 {
-    console.log('update snake');
+    for (let i = snakeBody.length - 2; i >= 0; i--)
+    {
+        snakeBody[i + 1] = { ...snakeBody[i]};
+    }
 }
 
 // Draw/Render the snake
