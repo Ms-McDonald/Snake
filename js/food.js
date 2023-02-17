86a48d42f70cb454fc98c7b76c65fc708319f32e
Ashley McDonald <ashley.mcdonald@frhsd.com>
2023-02-17 09:13:25 -0500

    33:48 - Students will finish for hw

---
+++ js/food.js
@@ -0,0 +1,24 @@
+import {onSnake, expandSnake } from '.snake.js';
+
+let food = { x: 10, y: 1 };
+const EXPANSION_RATE = 1;
+
+// Update the snake
+export function update()
+{
+    if(onSnake(food))
+   {
+    expandSnake(EXPANSION_RATE);
+    food = { x: 20, y: 10 };
+   }
+}
+
+// Draw/Render the snake
+export function draw(gameBoard)
+{
+    const foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food');
+    gameBoard.appendChild(foodElement);
+}
--- js/snake.js
+++ js/snake.js
@@ -1,9 +1,10 @@
 import { getInputDirection } from "./input.js";
 
-export const SNAKE_SPEED = 1;
+export const SNAKE_SPEED = 5;
 const snakeBody = [
     { x : 11, y : 11 }
 ];
+let newSegments = 0;
 
 // Update the snake
 export function update()
@@ -28,4 +29,18 @@ export function draw(gameBoard)
         snakeElement.classList.add('snake');
         gameBoard.appendChild(snakeElement);
     })
+}
+
+export function expandSnake(amount) {
+    newSegments += amount;
+}
+
+export function onSnake(position) {
+    return snakeBody.some(segment => {
+        return equalPositions(segment, position)
+    })
+}
+
+function equalPositions(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y;
 }
